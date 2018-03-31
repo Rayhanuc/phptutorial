675c36c8bcee4b6d72ceca5f11bb8fabaa0f083c
Rayhanuc <kj\hf_yu9i[wehr>
2018-03-31 21:40:02 +0600

    project luckyr and Admin Dashboard
    project luckyr saperated and included header, sidebar and footer. Project Admin Dashboard saperated and included header, sidebar, breadecumb and footer.

---
+++ luckyr/admin/function/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function get_header(){
+	include("includes/header.php");
+}
+
+function get_sidebar(){
+	include("includes/sidebar.php");
+}
+
+function get_breadcumb(){
+	include("includes/breadcumb.php");
+}
+
+function get_footer() {
+	include("includes/footer.php");
+}
+
+?>
---
+++ luckyr/admin/index.php
@@ -0,0 +1,23 @@
+<?php require_once("function/functions.php") ?>
+
+<?php
+
+	get_header();
+
+	get_sidebar();
+
+	get_breadcumb();
+
+	get_footer();
+
+
+
+
+
+    
+    // include('includes/header.php');
+    // include('includes/sidebar.php');
+    // include('includes/breadcumb.php');    
+    // include('includes/footer.php');
+
+?>
